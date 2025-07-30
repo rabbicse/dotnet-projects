c84de28904c06dee2315075804a0627b3aa4f3f1
Md. Mehedi Hasan Rabbi <mehedi.rabbi@asa-international.com>
2025-07-30 14:07:42 +0600

    Add project with mutex example

---
+++ MutexWpfExample/MutexWpfExample/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace MutexWpfExample;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private const string AppName = "MutexExample";
+    private static Mutex? _mutex;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // If it's not the new instance, exit the application
+        if (!IsNewInstance())
+        {
+            MessageBox.Show(
+                "Another instance of the application is already running.",
+                "Instance Check by Mutex!",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+            App.Current.Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    private static bool IsNewInstance()
+    {
+        _mutex = new Mutex(true, AppName, out var newInstance);
+        Debug.WriteLine("Checking for another instance of mutex : " + newInstance);
+        return newInstance;
+    }
+}
+
